66542adf09ee79f59a3aad40666834100606a9bb
BrandonXu360 <brandon.xu2024@gmail.com>
2023-02-15 01:04:17 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,48 @@
+let img;
+let cnv;
+
+function preload() {
+  //img = loadImage("assets/walking1.png");
+  img = loadImage("assets/walking1.png");
+}
+
+function keyPressed(){
+  if (key === 's'){
+    saveCanvas("portrait.png");
+  }
+}
+
+function setup() {
+  cnv = createCanvas(img.width, img.height);
+
+  clear();
+  //print(img.width, img.height);
+  let newCanvasX = (windowWidth - img.width)/2; 
+  let newCanvasY = (windowHeight - img.height)/2;
+
+  cnv.position(newCanvasX, newCanvasY);
+
+  //access the pixel information of the image
+  for(let col = 0; col < img.width; col+=2) {
+    for(let row = 0; row < img.height; row+=2) {
+      let xPos = col;
+      let yPos = row;
+      let c = img.get(xPos, yPos);
+      push();
+
+      translate(xPos, yPos);
+      rotate(radians(random(360)));
+      noFill();
+
+      strokeWeight(random(3));
+      stroke(color(c));
+
+      curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), 
+      random(10), random(80), cos(yPos) * sin(xPos) * random(140), 
+      cos(xPos) * sin(xPos) * 50);
+
+      pop();
+    }
+  }
+}
+
